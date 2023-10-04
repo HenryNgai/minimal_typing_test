3826c18f7d60d5d11140a88a4f699dc2075fabea
Henry Ngai <Henryngai98@Gmail.com>
2023-10-04 17:04:29 -0400

    Initial code push

---
+++ src/main/java/com/typing_test/minimal_typing_test/MinimalTypingTestApplication.java
@@ -0,0 +1,13 @@
+package com.typing_test.minimal_typing_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalTypingTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinimalTypingTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/typing_test/minimal_typing_test/MinimalTypingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.typing_test.minimal_typing_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinimalTypingTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
